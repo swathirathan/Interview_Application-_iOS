5c79c2819027cda55316c34015d1aaac55355fb2
Swathi Rathan <swathirathan@Swathis-MacBook-Air.local>
2015-07-20 01:43:10 -0400

    Initial Commit

---
+++ Interview_Application/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Interview_Application
+//
+//  Created by Swathi Rathan on 7/20/15.
+//  Copyright (c) 2015 Swathi Rathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Interview_Application/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Interview_Application
+//
+//  Created by Swathi Rathan on 7/20/15.
+//  Copyright (c) 2015 Swathi Rathan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
